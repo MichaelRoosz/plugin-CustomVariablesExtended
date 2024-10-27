25f5c69d34a048d923a365c073dbbeaaea59051a
michael.roosz <michael.roosz@check24.de>
2024-10-27 08:33:57 +0100

    Add license

--- Reports/GetCustomVariables.php
+++ Reports/GetCustomVariables.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link http://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
 namespace Piwik\Plugins\CustomVariablesExtended\Reports;
 
 use Piwik\DataTable;
